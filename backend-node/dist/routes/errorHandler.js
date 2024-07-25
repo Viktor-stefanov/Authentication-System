45b6bb036719603ca91977eb7fba70e502a9da0a
Viktor <viktor.stefanov02@gmail.com>
2024-07-25 15:14:02 +0300

    remove dist directory

--- backend-node/dist/routes/errorHandler.js
+++
@@ -1,7 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-    console.log(err.stack); // Log the error stack
-    res.status(500).json({
-        message: err.message || "Internal server error",
-    });
-};
-export default errorHandler;
